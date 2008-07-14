ec1e51913eae6ec5419b8e283108b79b2beb6bb3
riobard <me@riobard.com>
2008-07-15 03:22:19 +0800

    Cleaned up solutions for Problem 14
    Deleted the old procedure, leaving only the memorized solution

--- 014.py
+++ 014.py
@@ -1,19 +1,3 @@
-'''
-# This is gonna take a while
-m, mm   = 1, 0
-for n in range(2,10**6):
-    k   = n
-    i   = 1
-    while n != 1:
-        n   = 3*n + 1 if n % 2 else n/2
-        i   += 1
-    if i > m:
-        m   = i
-        mm  = k
-
-print mm, m
-'''
-
 from utils import timex
 
 d   = {1: 1}
