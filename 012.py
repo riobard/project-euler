2438db75f34f2d14b20249176c8d873c56b74926
riobard <me@riobard.com>
2008-09-03 22:14:58 +0800

    Finished Problem 12

--- 012.py
+++ 012.py
@@ -1,13 +1,33 @@
+# for more information about this problem, check
+# http://mathschallenge.net/index.php?section=faq&ref=number/number_of_divisors
+#
+# n can be uniquely represented by a^x * b^y * c^z, where a, b, c are primes
+# let d(n) = the number of divisors of n
+# then d(n) = (x+1) * (y+1) * (z+1)
 
-def factor(n):
-    return [x for x in range(1, n//2 + 1) if not (n % x)] + [n]
+from euler import primefactors, product
+from itertools import count
+from collections import defaultdict
+
+def groupcount(sq):
+    ''' Accept a sequence, sq, and count its elements by group '''
+    d   = defaultdict(int)
+    for each in sq:
+        d[each] += 1
+    return d
+
+def trinum(n):
+    ''' Return the n-th triangle number '''
+    return n*(n+1)/2
+
+def trinumgen():
+    ''' Generate the sequence of triangle numbers '''
+    for n in count(1):
+        yield trinum(n)
 
 
-from euler import primefactors as factorize
-print list(factorize(2**10))
-
-
-for i in range(1, 10**2):
-    fs  = list(factorize(i))
-    ff  = factor(i)
-    print len(ff),'\t', i,'\t', fs, '\t', ff
+for trin in trinumgen():
+    primefs = primefactors(trin)
+    if product(map(lambda x: x+1, groupcount(primefs).values())) > 500:
+        print trin
+        break
