ee85f292864928046ecfd47011b406e8cfce09e8
riobard <me@riobard.com>
2008-09-28 17:51:04 -0230

    Optimize solution to Problem 48
    Use pow(x,y,z) function to speed up calculation.

--- 048.py
+++ 048.py
@@ -1,2 +1,4 @@
-print str(sum(x**x for x in range(1,1001)))[-10:]
-print sum(x**x for x in range(1,1001)) % 10**10
+#print str(sum(x**x for x in range(1,1001)))[-10:]
+#print sum(x**x for x in range(1,1001)) % 10**10
+L   = 10**10
+print sum(pow(x,x,L) for x in range(1,1001)) % L
