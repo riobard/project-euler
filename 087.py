e35be1c26b566a52635053ae20ea78ddd92e50c4
Riobard <me@riobard.com>
2010-11-21 15:01:31 -0330

    Solved 087

---
+++ 087.py
@@ -0,0 +1,23 @@
+from euler import prime
+from itertools import takewhile
+
+
+def run():
+    MAX = 50000000
+
+    p   = list(takewhile(lambda n: n <= MAX**.5, prime))
+    p2  = [n**2 for n in p]
+    p3  = [n**3 for n in p]
+    p4  = [n**4 for n in p]
+
+    s   = set()
+    for a2 in p2:
+        if a2 > MAX: break
+        for b3 in p3:
+            if a2 + b3 > MAX: break
+            for c4 in p4:
+                if a2 + b3 + c4 > MAX: break
+                s.add(a2+b3+c4)     # remove duplicates in a set
+
+
+assert 1097343 == len(s)
