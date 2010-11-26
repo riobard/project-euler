22da14c4234afab47901e69066428461cfa0bc19
Riobard <me@riobard.com>
2010-11-26 19:39:27 -0330

    Solved 064

---
+++ 064.py
@@ -0,0 +1,32 @@
+'''
+http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion 
+'''
+
+def CFE(s):
+    ' Continued fraction expansion of sqrt(s) '
+
+    m   = 0
+    d   = 1
+    a0  = int(s**.5)
+    a   = a0
+    
+    encountered = set([(m, d, a)])
+    rs  = [a0]
+
+    while True:
+
+        m   = d * a - m
+        d   = (s - m**2) / d
+        if d == 0: break    # a perfect square
+        a   = int(a0 + m) // d
+
+        if (m, d, a) in encountered:
+            break
+        else:
+            rs  += [a]
+            encountered.add((m, d, a))
+
+    return rs
+
+
+print sum(1 for n in range(10000+1) if len(CFE(n)) > 1 and len(CFE(n)) % 2==0) 
